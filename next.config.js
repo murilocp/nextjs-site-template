eb041a79cae58840062fc893e9ff205e3fb8886c
Murilo Picinato <50213850+murilocp@users.noreply.github.com>
2022-01-17 20:00:08 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-const withImages = require('next-images');
+module.exports = {
+  reactStrictMode: true,  
+  images: {
+    domains: ['images.prismic.io']
+  },
 
-module.exports = withImages({
-  esModule: true,
-});
+};
